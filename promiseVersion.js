34a105852a7feb308ac11c3cd494f9ab42029d52
Amanda <amandeee77@yahoo.com>
2025-06-11 12:42:16 -0400

    Add summer chores functions with promises for asynchronous tasks

---
+++ promiseVersion.js
@@ -0,0 +1,65 @@
+function mowYard(name) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`${name} mowed the yard.`);
+            resolve(true);
+        }, 2000);
+    });
+}
+
+function weedEat(name) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Math.random() > 0.2) {
+                console.log(`${name} finished using the weed eater.`);
+                resolve(true);
+            } else {
+                console.log(`${name} fell asleep after mowing the yard.`);
+                reject(false);
+            }
+        }, 1500);
+    });
+}
+
+function trimHedges(name) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Math.random() > 0.3) {
+                console.log(`${name} finished trimming the hedges.`);
+                resolve(true);
+            } else {
+                console.log(`${name} fell asleep after weed eating the yard.`);
+                reject(false);
+            }
+        }, 1000);
+    });
+}
+
+function collectWood(name) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Math.random() > 0.4) {
+                console.log(`${name} finished collecting wood.`);
+                resolve(true);
+            } else {
+                console.log(`${name} fell asleep after trimming the hedges.`);
+                reject(false);
+            }
+        }, 2500);
+    });
+}
+
+function waterGarden(name) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Math.random() > 0.5) {
+                console.log(`${name} finished watering the garden.`);
+                resolve(true);
+            } else {
+                console.log(`${name} fell asleep after collecting wood.`);
+                reject(false);
+            }
+        }, 500);
+    });
+}
+doSummerChores("Amanda");
